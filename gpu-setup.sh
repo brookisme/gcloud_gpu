fcc9afc90ce9f11aa674da3b41e1a0c59f142190
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-06-06 20:21:47 -0700

    colored git branch prompt

--- gpu-setup.sh
+++ gpu-setup.sh
@@ -61,13 +61,43 @@ lsports(){ sudo lsof -n -i | grep $1 | grep LISTEN ; }
 alias gpo='git push origin'
 alias gs='git status'
 alias gc='git commit -am'
-# Git branch in prompt.
-parse_git_branch() {
 
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+# Configure colors, if available. (http://vvv.tobiassjosten.net/bash/dynamic-prompt-with-git-and-ansi-colors/)
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+  c_reset='\[\e[0m\]'
+  c_user='\[\e[1;36m\]'
+  c_path='\[\e[0;33m\]'
+  c_git_clean='\[\e[1;32m\]'
+  c_git_dirty='\[\e[0;31m\]'
+else
+  c_reset=
+  c_user=
+  c_git_cleancleann_path=
+  c_git_clean=
+  c_git_dirty=
+fi
 
+# Function to assemble the Git parsingart of our prompt.
+git_prompt ()
+{
+  if ! git rev-parse --git-dir > /dev/null 2>&1; then
+    return 0
+  fi
+
+  git_branch=$(git branch 2>/dev/null | sed -n '/^\*/s/^\* //p')
+
+  if git diff --quiet 2>/dev/null >&2; then
+    git_color="$c_git_clean"
+  else
+    git_color="$c_git_dirty"
+  fi
+
+  echo "[$git_color$git_branch${c_reset}]"
 }
-export PS1='\W\[\033[00m\]|\$(parse_git_branch)\[\033[00m\] $ '
+
+# Thy holy prompt.
+PROMPT_COMMAND='PS1="${c_user}\u${c_reset}@${c_user}\h${c_reset}:${c_path}\w${c_reset}$(git_prompt)\$ "'
+
 
 #
 # ML DIRS
