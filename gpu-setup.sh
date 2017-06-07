7fdf0dedd15a22a71b7e3300d4a94636fe29fc8c
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-06-06 20:56:32 -0700

    fix prompt

--- gpu-setup.sh
+++ gpu-setup.sh
@@ -62,7 +62,8 @@ alias gpo='git push origin'
 alias gs='git status'
 alias gc='git commit -am'
 
-# Configure colors, if available. (http://vvv.tobiassjosten.net/bash/dynamic-prompt-with-git-and-ansi-colors/)
+
+# Configure colors, if available.
 if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
   c_reset='\[\e[0m\]'
   c_user='\[\e[1;36m\]'
@@ -79,24 +80,24 @@ fi
 
 # Function to assemble the Git parsingart of our prompt.
 git_prompt ()
-{
+{ 
   if ! git rev-parse --git-dir > /dev/null 2>&1; then
     return 0
   fi
-
+  
   git_branch=$(git branch 2>/dev/null | sed -n '/^\*/s/^\* //p')
-
+  
   if git diff --quiet 2>/dev/null >&2; then
-    git_color="$c_git_clean"
+    git_color=\"$c_git_clean\"
   else
-    git_color="$c_git_dirty"
+    git_color=\"$c_git_dirty\"
   fi
-
-  echo "[$git_color$git_branch${c_reset}]"
+  
+  echo \"[$git_color$git_branch${c_reset}]\"
 }
 
 # Thy holy prompt.
-PROMPT_COMMAND='PS1="${c_user}\u${c_reset}@${c_user}\h${c_reset}:${c_path}\w${c_reset}$(git_prompt)\$ "'
+PROMPT_COMMAND='PS1=\"${c_user}\u${c_reset}@${c_user}\h${c_reset}:${c_path}\w${c_reset}$(git_prompt)\$ \"'
 
 
 #
